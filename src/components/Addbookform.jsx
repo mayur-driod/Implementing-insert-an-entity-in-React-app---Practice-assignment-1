c58e2e2b226418f3a400b58a090b632bc1314a92
Mayur K Setty <settymayurk@gmail.com>
2025-03-23 08:59:29 +0530

    Update Addbookform.jsx

--- src/components/Addbookform.jsx
+++ src/components/Addbookform.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './AddBookForm.css';
+import books from '../booksData';
 
 const AddBookForm = () => {
   const navigate = useNavigate();
+  const [formdata, setformdata] = useState({
+    Title: '',
+    Author: '',
+    Description: '',
+    Image: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    books.push({
+      title: formdata.Title,
+      author: formdata.Author,
+      description: formdata.Description,
+      coverImage: formdata.Image
+    });
+
+    setformdata({
+    Title: '',
+    Author: '',
+    Description: '',
+    Image: '',
+    });
+
+    navigate('/');
+
   };
 
   return (
@@ -15,19 +37,50 @@ const AddBookForm = () => {
         <h2>Add New Book</h2>
         <label>
           Title:
-          <input type="text" placeholder="Enter book title" required />
+          <input
+            value={formdata.Title}
+            onChange={(e) =>
+              setformdata({ ...formdata, Title: e.target.value })
+            }
+            type="text"
+            placeholder="Enter book title"
+            required
+          />
         </label>
         <label>
           Author:
-          <input type="text" placeholder="Enter author's name" required />
+          <input
+            value={formdata.Author}
+            onChange={(e) =>
+              setformdata({ ...formdata, Author: e.target.value })
+            }
+            type="text"
+            placeholder="Enter author's name"
+            required
+          />
         </label>
         <label>
           Description:
-          <textarea placeholder="Enter book description" required></textarea>
+          <textarea
+            value={formdata.Description}
+            onChange={(e) =>
+              setformdata({ ...formdata, Description: e.target.value })
+            }
+            placeholder="Enter book description"
+            required
+          ></textarea>
         </label>
         <label>
           Cover Image URL:
-          <input type="url" placeholder="Enter image URL" required />
+          <input
+            value={formdata.Image}
+            onChange={(e) =>
+              setformdata({ ...formdata, Image: e.target.value })
+            }
+            type="url"
+            placeholder="Enter image URL"
+            required
+          />
         </label>
         <div className="form-buttons">
           <button type="submit" className="submit-button">
